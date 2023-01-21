7bf61106d4a3b9f78c7a0b2eab2cb56d12fa0b5f
Joshua Nash <joshuangola@gmail.com>
2023-01-21 15:34:02 -0700

    solved

--- gitInfo.js
+++ gitInfo.js
@@ -8,7 +8,7 @@
     Create a variable called 'gitDefinition'.
     It should be a string containing your best definition of what Git is.
 */
-
+const gitDefinition = "Git allows you to keep track of changes to your code and collaborate with others. It allows you to save versions of your code, revert to previous versions, and merge changes made by different users."
 //CODE HERE
 
 //////////////////PROBLEM 2////////////////////
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+const gitHubDefinition = "GitHub Website that uses Git. By using Git it makes it very easy to collaborate with others. Save versions of your code and revert to previous versions. GitHub also has a great community of developers that work on many projects"
 //////////////////PROBLEM 3////////////////////
 /*
     Create a variable called 'gitInitDefinition'.  
@@ -26,7 +26,7 @@
 */
 
 //CODE HERE
-
+const gitInitDefinition = "git init initializes a new Git repository in the current directory. It creates a new subdirectory called .git which contains all the necessary files for control."
 //////////////////PROBLEM 4////////////////////
 /*
     Create a variable called 'gitCloneDefinition'.  
@@ -34,7 +34,7 @@
 */
 
 //CODE HERE
-
+const gitCloneDefinition = "git clone is a command that allows you to copy an existing Git repository from a remote location to your Computer. In this class we have been using it to clone from GitHub. It creates a new directory with the same name as the repository and copies all the necessary files for control."
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,8 +42,7 @@
 */
 
 //CODE HERE
-
-//////////////////PROBLEM 6////////////////////
+const gitStatusDefinition = "git status is a command that shows the current status of your repository, including which files have been modified, added, or deleted, and whether or not they are ready for a commit.
 /*
     Create a variable called 'gitAddDefinition'.  
     It should be a string containing your best definition of what 'git add' does.
@@ -53,7 +52,8 @@
 */
 
 //CODE HERE
-
+const gitAddDefinition = "git add is a command that allows you to select which files you want to include in your next commit. ";
+const gitAddCode = "git add .";
 //////////////////PROBLEM 7////////////////////
 /*
     Create a variable called 'gitCommitDefinition'.  
@@ -64,7 +64,8 @@
 */
 
 //CODE HERE
-
+const gitCommitDefinition = "git commit is a command that saves changes to the local repository. It creates a new snapshot of the project's files and stores it in the repository's history. A commit message is required to describe the changes made.";
+const gitCommitCode = "git commit -m 'initial commit'";
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +74,4 @@
 */
 
 //CODE HERE
+const gitPushDefinition = "git push is a command that sends your committed changes to a remote repository like GitHub. It updates the remote repository with your local commits, making your changes visible to others.";
